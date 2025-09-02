c1c98713c89ef9533ff4441f1f909de42226a8f1
胜\83903 <js.jingsheng.li@foxmail.com>
2025-09-02 16:42:29 +0800

    consecutive

---
+++ consecutive_numbers/consecutive.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	array := []int{1, 1, 1, 1, 2, 2, 5, 5, 6, 7, 3, 3, 4, 5, 7, 7, 8, 8, 9, 10}
+	consecutiveNumbers := findConsecutiveNumbers(array)
+	fmt.Println(consecutiveNumbers)
+}
+
+func findConsecutiveNumbers(array []int) []int {
+	consecutiveNumbers := []int{}
+
+	currentNumber := array[0]
+	currentCount := 0
+
+	for _, number := range array {
+		if currentNumber == number {
+			currentCount++
+		} else {
+			consecutiveNumbers = append(consecutiveNumbers, currentCount)
+			currentNumber = number
+			currentCount = 1
+		}
+	}
+
+	consecutiveNumbers = append(consecutiveNumbers, currentCount)
+	return consecutiveNumbers
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module example.com/algorithm
+
+go 1.25.0
