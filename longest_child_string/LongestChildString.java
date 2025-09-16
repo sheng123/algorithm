d301d9632b8bd0c5ec104e2cd9527216b3181287
胜\83903 <js.jingsheng.li@foxmail.com>
2025-09-16 11:20:15 +0800

    length of longest substring

--- longest_child_string/LongestChildString.java
+++ longest_child_string/LongestChildString.java
@@ -1,10 +1,14 @@
 package longest_child_string;
 
+import java.util.HashSet;
+import java.util.Set;
+
 // 找出字符串中最长的无重复子串
 public class LongestChildString {
     public static void main(String[] args) {
         String str = "abcabcdefcaabb";
         System.out.println(longestChildString(str));
+        System.out.println("max lenght is: " + maxLength(str));
     }
 
     public static String longestChildString(String str) {
@@ -33,5 +37,26 @@ public class LongestChildString {
             right++;
         }
         return longestChildString;
+    }
+
+    // 另外一种方式获取最长无重复子串的长度
+    public static int maxLength(String str) {
+        int left = 0;
+        int right = 0;
+        int maxLength = 0;
+        Set<Character> set = new HashSet<>();
+
+        while (right < str.length()) {
+            char currentChar = str.charAt(right);
+            if (!set.contains(currentChar)) {
+                set.add(currentChar);
+                maxLength = Math.max(maxLength, right - left + 1);
+                right++;
+            } else {
+                set.remove(str.charAt(left));
+                left++;
+            }
+        }
+        return maxLength;
     }
 }
